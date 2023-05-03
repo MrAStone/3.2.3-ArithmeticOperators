91be58d9f5c7fd788757c1f7844bfc79c57977e8
MrAStone <astone@heckgrammar.co.uk>
2023-05-03 15:03:57 +0100

    Add project files.

---
+++ 3.2.3-ArithmeticOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2._3_ArithmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add two integers
+            int num1,num2;
+            num1 = 12; num2 = 13;
+            int num3 = num1 + num2;
+            Console.WriteLine(num3);    
+            //add two decimals
+            double dNum1, dNum2;
+            dNum1 = 3.1596;
+            dNum2 = 9.3642;
+            double dNum3 = dNum1 + dNum2;  
+            Console.WriteLine(dNum3);
+
+        }
+    }
+}
